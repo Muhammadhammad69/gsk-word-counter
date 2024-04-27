b169307f58df01ff72e2a62d11cf52352d267770
fareeha03 <hammadak69@gmail.com>
2024-04-27 15:43:07 +0500

    adding complete project of word counter

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+const user = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your paragraph to count the word"
+    }
+]);
+let userAnswer = user.sentence.trim().split(" "); //trim k method string me say start or end k whitespace khtam krta hain or split k method break krata hain jo hum os ko bata hain k kaha say break krna hain or eik array return krta hain.  
+// console.log(userAnswer);
+for (let i = 0; i < userAnswer.length; i++) {
+    if (userAnswer[i] === "") {
+        userAnswer.splice(i, 1);
+        // console.log("correct",);
+        i--;
+    }
+}
+console.log(`Your total words in a paragraph is ${userAnswer.length}`);
